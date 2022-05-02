070813d40a914b6cb6de40c04e8761c0ec7947df
NingyuanWeng <ningyuan0726@gmail.com>
2022-05-02 15:40:10 -0500

    UPDATE

--- R/myddt.R
+++ R/myddt.R
@@ -1,6 +1,6 @@
 #' @title filtering data
 #' @param df  data frame
-#' @param cond  condition for filter
+#' @param SPECIES  condition for filter
 #' @importFrom dplyr '%>%' filter
 #' @importFrom ggplot2 ggplot geom_point aes_string geom_smooth labs
 #' @importFrom utils write.csv
@@ -8,21 +8,22 @@
 #' @export
 #'
 #' @examples
-#' \dontrun{myddt(ddt,SPECIES == "CCATFISH")}
-myddt <- function(df, cond){
+#' \dontrun{myddt(ddt,SPECIES = "CCATFISH")}
+myddt <- function(df, SPECIES){
+  RIVER <-  WEIGHT <- LENGTH <- NULL
+  newdf <- df %>% filter(SPECIES == {{SPECIES}})                 # data frame for only a specific SPECIES
+  g <- ggplot(newdf, aes_string(x = "LENGTH",y = "WEIGHT")) +    # plot LENGTH Vs WEIGHT
+    geom_point(aes_string(color = "RIVER" )) +                   # data points are colored according to the RIVER variable
+    geom_smooth(formula = y~x +I(x^2), method = "lm")+           # a quadratic curve
+    labs(title="Ningyuan Weng")                                  # my name appear on the title
+  print(g)                                                       # print the plot
 
-  df1 <- df %>% filter({{cond}}) # Note the use of {{}}
-  g <- ggplot(df1, aes_string(x = "LENGTH",y = "WEIGHT")) + # Note the use of aes_string
-    geom_point(aes_string(color = "RIVER" )) +
-    geom_smooth(formula = y~x +I(x^2), method = "lm")+
-    labs(title="Ningyuan Weng")
-  print(g)
-
-  if("CCATFISH" %in% df1$SPECIES){
-    write.csv(df1,'CCATFISH.csv')
+  list(head(df),head(newdf),(table(df$RIVER)/length(df$RIVER)))
+  if("CCATFISH" %in% newdf$SPECIES){                             #save the data frame to the working directory as a csv file called LvsWforCCATFISH.csv
+    write.csv(newdf,'LvsWforCCATFISH.csv')
   }
-  if("SMBUFFALO" %in% df1$SPECIES){
-    write.csv(df1,'SMBUFFALO.csv')
+  if("SMBUFFALO" %in% newdf$SPECIES){                            #save the data frame to the working directory as a csv file called LvsWforSMBUFFALO.csv
+    write.csv(newdf,'LvsWforSMBUFFALO.csv')
   }
 
 }
